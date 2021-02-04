4187465cb983b358aee8c149497c8b8480f12269
Pauline Narvas <pauline@paulinenarvas.com>
2021-02-04 11:12:02 +0000

    update get-started.sh

--- get-started.sh
+++ get-started.sh
@@ -1,14 +1,22 @@
+# get dotfiles repo
 git clone https://github.com/pawlean/dotfiles.git
 cd dotfiles
 
+# run installation
 sh install.sh
 
-ln -sv ~/Volumes/R/Documents/dev/dotfiles/.alias ~
+# start symlink
+ln -sv ~/dev/dotfiles/.alias ~
 source .alias
 
-ln -sv ~/Volumes/R/Documents/dev/dotfiles/.functions ~
+ln -sv ~/dev/dotfiles/.functions ~
 source .functions
 
-ln -sv ~/Volumes/R/Documents/dev/dotfiles/.macos ~
+ln -sv ~/dev/dotfiles/.macos ~
 source .macos
 
+ln -sv ~/dotfiles/.oh-my-zsh/oh-my-zsh.sh ~
+source .oh-my-zsh/oh-my-zsh.sh
+
+ln -sv ~/dotfiles/.oh-my-zsh/themes/agnoster.zsh-theme ~
+source themes/agnoster.zsh-theme
