31075df04aee78b54fff92a10c61d9a842d2edb4
Pauline Narvas <paulinenarvas@Paulines-MacBook-Air.local>
2021-05-02 07:06:07 +0100

    update get-started guide

--- get-started.sh
+++ get-started.sh
@@ -14,3 +14,6 @@ ln -sv ~/dotfiles/.macos ~
 
 # run symlinks
 sh source.sh
+
+# set up ssh keys on gh
+# https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh
